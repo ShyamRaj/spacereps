c70646811227e990e4e22411f6f405356562e8fe
Shyam <shyam.rajendran@kroger.com>
2016-06-16 12:52:40 -0400

    Added create.html with does nothing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
+var path = require('path');
 var bodyParser = require("body-parser");
 var memo_create = require('./api/memo_create');
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
